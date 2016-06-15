3c2470d98330d95e3f865f475156537dd514c424
Victor Mehmeri <vmehmeri@hotmail.com>
2016-06-15 11:25:16 -0500

    Bug fixes

--- sfc-py/sfc/sfc_agent.py
+++ sfc-py/sfc/sfc_agent.py
@@ -43,29 +43,36 @@ app = flask.Flask(__name__)
 logger = logging.getLogger(__file__)
 nfq_classifier = classifier.NfqClassifier()
 sfc_globals = _sfc_globals.sfc_globals
-docker_cli = Client(base_url='unix://var/run/docker.sock')
+docker_client = Client(base_url='unix://var/run/docker.sock')
 
-def create_sf_container():
-    container = docker_cli.create_container(image='vmehmeri/sf')
+def create_sf_container(image='vmehmeri/sf',name='sf'):
+    container = docker_client.create_container(image=image, name=name, detach=False) #command='sudo python /home/root/vxlan_tool.py -i eth0 -d forward -v on')
     return container
 
 def start_sf_container(container):
-    response = docker_cli.start(container=container.get('Id'))
+    response = docker_client.start(container=container)
     return response
 
 def stop_sf_container(container):
-    response = docker_cli.stop(container=container.get('Id'))
+    response = docker_client.stop(container=container.get('Id'))
     return response
 
 def remove_sf_container(container):
-    response = docker_cli.remove_container(container=container.get('Id'))
+    response = docker_client.remove_container(container=container.get('Id'))
     return response
+
+def attach_to_sf_container(container):
+    docker_client.attach(container=container, stdout=True, stderr=True)
 
 def test_start_sf_container():
     print ("Creating container")
     ctr = create_sf_container()
+    print (ctr)
     print ("Starting container")
     print (start_sf_container(ctr))
+    #attach_to_sf_container(ctr)
+    input('Press any key to continue: ')
+
     print ("Stopping container")
     print (stop_sf_container(ctr))
     print ("Removing container")
@@ -669,5 +676,5 @@ def main():
 
 
 if __name__ == "__main__":
-    #test_start_sf_container()
-    main()
+    test_start_sf_container()
+    #main()
