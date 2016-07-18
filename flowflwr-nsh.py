748aa3d626bcc18e8efe22af285cddb645b03087
Victor Mehmeri <vime@fotonik.dtu.dk>
2016-07-18 16:17:15 -0500

    Fixed flwflr-nsh

--- flowflwr-nsh.py
+++ flowflwr-nsh.py
@@ -42,9 +42,9 @@ def dump_flows(bridge_name, protocol='OpenFlow10'):
 
 
 while True:
-    out1, n_packets1 = dump_flows('s1', 'OpenFlow10')
+    out1, n_packets1 = dump_flows('br-sfc', 'OpenFlow13')
     sleep(POLLING_INTERVAL)
-    out2, n_packets2 = dump_flows('s1', 'OpenFlow10')
+    out2, n_packets2 = dump_flows('br-sfc', 'OpenFlow13')
 
     out2arr = out2.splitlines()
 
@@ -55,7 +55,7 @@ while True:
         if len(out2arr) == len(n_packets1arr) and n_packets1arr[i] != n_packets2arr[i]:
             if "set_nsp" in out2arr[i]:
                 print bcolors.HEADER + "[CLSF FLOW HIT]" + bcolors.ENDC, out2arr[i]
-            else if "nsp=" in out2arr[i]:
+            elif "nsp=" in out2arr[i]:
                 print bcolors.OKBLUE + "[SFF FLOW HIT]" + bcolors.ENDC, out2arr[i]
             else:
                 print bcolors.OKGREEN + "[FLOW HIT]" + bcolors.ENDC, out2arr[i]
